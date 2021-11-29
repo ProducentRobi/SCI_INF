386ef8446d4c69478641c9c5d4524814254ecd77
Robert <robson.urbanski12@wp.pl>
2021-11-29 21:17:01 +0100

    Added (change) PracaNaLekcji - RSA i klasy

--- PracaNaLekcji - RSA i klasy/szyfr.cpp
+++ PracaNaLekcji - RSA i klasy/szyfr.cpp
@@ -1,51 +1,125 @@
+﻿#include "szyfr.h"
 #include <iostream>
 #include <string>
-#include "szyfr.h"
 
-using namespace std;
+string wlasny_tekst_wejscie, wlasny_tekst_wyjscie;
+int cyfra;
 
-
-int main()
+string szyfr::szyfr_cezara(string wlasny_tekst_wejscie)
 {
-    int wybor_rodzaju_szyfrowania;
-    string wlasny_tekst_wejscie, wlasny_tekst_wyjscie;
-    szyfr szyfrik;
-    cout << "Podaj swoj tekst: ";
-    getline(cin, wlasny_tekst_wejscie);
-    cout << "Wybierz rodzaj szyfrowania (podaj liczbe)" << endl;
-    cout << "1. Szyf cezara" << endl;
-    cout << "2. Szyfr przestawieniowy" << endl;
-    cout << "3. Szyfr Cezara i przestawieniowy" << endl;
-    cout << "4. Odszyfruj szyfr cezara" << endl;
-    cout << "5. RSA" << endl;
-    cin >> wybor_rodzaju_szyfrowania;
-    if (wybor_rodzaju_szyfrowania == 1)
+    string string_dane;
+    cout << "wypisz o ile chcesz przesunac szyfr (przedzial 1-26): ";
+    cin >> cyfra;
+
+    for (int i = 0; i < wlasny_tekst_wejscie.size(); i++)
     {
-        wlasny_tekst_wyjscie = szyfrik.szyfr_cezara(wlasny_tekst_wejscie);
-        cout << "Tekst zaszyfrowany to: " << wlasny_tekst_wyjscie << endl;
+        if (wlasny_tekst_wejscie[i] > 96)
+        {
+            char dane;
+            dane = wlasny_tekst_wejscie[i] + cyfra;
+            if (dane > 124)
+            {
+                dane = (dane % 123) + 97;
+
+            }
+            string_dane = string_dane + dane;
+        }
+        else
+        {
+            string_dane = string_dane + wlasny_tekst_wejscie[i];
+        }
     }
-    else if (wybor_rodzaju_szyfrowania == 2)
+
+    return string_dane;
+}
+
+string szyfr::przestawienie(string wlasny_tekst_wejscie)
+{
+    string string_dane;
+    char daneA;
+
+    for (int i = 0; i < wlasny_tekst_wejscie.size() - 1; i = i + 2)
     {
-        wlasny_tekst_wyjscie = szyfrik.przestawienie(wlasny_tekst_wejscie);
-        cout << "Tekst zaszyfrowany to: " << wlasny_tekst_wyjscie << endl;
+        swap(wlasny_tekst_wejscie[i], wlasny_tekst_wejscie[i + 1]);
+        daneA = wlasny_tekst_wejscie[i];
+        string_dane = string_dane + daneA;
+        daneA = wlasny_tekst_wejscie[i + 1];
+        string_dane = string_dane + daneA;
     }
-    else if (wybor_rodzaju_szyfrowania == 3)
+    if (wlasny_tekst_wejscie.size() % 2 == 1)
     {
-        wlasny_tekst_wyjscie = szyfrik.cezar_przestawienie(wlasny_tekst_wejscie);
-        cout << "Tekst zaszyfrowany to: " << wlasny_tekst_wyjscie << endl;
+        daneA = wlasny_tekst_wejscie[wlasny_tekst_wejscie.size() - 1];
+        string_dane = string_dane + daneA;
     }
-    else if (wybor_rodzaju_szyfrowania == 4)
+    return string_dane;
+}
+
+string szyfr::cezar_przestawienie(string wlasny_tekst_wejscie)
+{
+    string dane;
+
+    wlasny_tekst_wyjscie = szyfr_cezara(przestawienie(wlasny_tekst_wejscie));
+    return wlasny_tekst_wyjscie;
+}
+
+string szyfr::deszyfracja(string wlasny_tekst_wejscie)
+{
+    string string_dane;
+    cout << "Wypisz o ile chcesz przesunac szyfr (przedzial 1 - 26): ";
+    cin >> cyfra;
+    for (int i = 0; i < wlasny_tekst_wejscie.size(); i++)
     {
-        wlasny_tekst_wyjscie = szyfrik.deszyfracja(wlasny_tekst_wejscie);
-        cout << "Tekst wyjsciowy brzmi: " << wlasny_tekst_wyjscie << endl;
+        if (wlasny_tekst_wejscie[i] > 96)
+        {
+            char dane;
+            dane = wlasny_tekst_wejscie[i] - cyfra;
+            if (dane < 97)
+            {
+                char x;
+                x = (wlasny_tekst_wejscie[i] % 97) + 123;
+                x = x - cyfra;
+                dane = x;
+            }
+            string_dane = string_dane + dane;
+        }
+        else
+        {
+            string_dane = string_dane + wlasny_tekst_wejscie[i];
+        }
     }
-    else if (wybor_rodzaju_szyfrowania == 5)
-    {
-        wlasny_tekst_wyjscie = szyfrik.RSA(wlasny_tekst_wejscie);
-    }
-    else
-    {
-        cout << endl;
-        cout << "Wybrany zly rodzaj szyfrowania, uruchom program ponownie" << endl;
-    }
+    return string_dane;
+}
+
+int odwrotnosc_modulo(int a, int m)
+{
+    for (int y = 1; y < m; y++)
+        if (((a % m) * (y % m)) % m == 1)
+            return y;
+}
+
+string szyfr::RSA(string wlasny_tekst_wejscie)
+{
+    int liczba_pierwsza1, lizba_pierwsza2;
+    cout << "Podaj 1 liczbe pierwsza: ";
+    cin >> liczba_pierwsza1;
+    cout << "Podaj 1 liczbe pierwsza: ";
+    cin >> lizba_pierwsza2;
+    int l = liczba_pierwsza1 * lizba_pierwsza2;
+    int f_eulera = (liczba_pierwsza1 - 1) * (lizba_pierwsza2 - 1);
+    cout << "Podaj liczbe z przedzialu od 1 do " << z << endl;
+    cout << "Ta liczba i " << f_eulera << " powinny byc wzglednie pierwsze" << endl;
+    int e; 
+    cin >> e;
+    int d; 
+    d = odwrotnosc_modulo(e, f_eulera);
+
+    pair<int, int> publiczny;
+    publiczny.first = l;
+    publiczny.second = e;
+    cout << "Klucz publiczny: " << publiczny.first << " - " << publiczny.second << endl;
+    pair<int, int> prywatny;
+    prywatny.first = l;
+    prywatny.second = d;
+    cout << "Klucz prywatny: " << prywatny.first << " - " << prywatny.second << endl;
+    return " ";
 }
