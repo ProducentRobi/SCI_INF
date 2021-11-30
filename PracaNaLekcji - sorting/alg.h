cc09c96e1a3a5be1ed2b80f36b9f83ab2388cc34
Robert <robson.urbanski12@wp.pl>
2021-11-30 22:31:52 +0100

    Added PracaNaLekcji - sorting

---
+++ PracaNaLekcji - sorting/alg.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class alg
+{
+	public:
+		vector<int> exercise;
+		void see();
+		void random();
+		void bubbles();
+		void insert();
+};
